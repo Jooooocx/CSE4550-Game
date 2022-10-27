68eaf83c0a0f811a490d9826db3e289f60cbbf19
nyldones <115131000+nyldones@users.noreply.github.com>
2022-10-26 19:09:39 -0700

    Inventory Added
    needs more work

---
+++ 2DTopDownProject/Assets/Sprites/ItemContainer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ItemSlot
+{
+    public Item item;
+    public int count;
+    //public int slot;
+}
+
+[CreateAssetMenu(menuName ="Data/Item Container")]
+public class ItemContainer : ScriptableObject
+{
+    public List<ItemSlot> slots;
+}
