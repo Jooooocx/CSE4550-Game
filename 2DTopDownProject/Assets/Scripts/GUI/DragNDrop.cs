358fe017ec425387c5c44664d45be018d4004f1d
dmier12 <113485490+dmier12@users.noreply.github.com>
2022-10-27 04:04:29 -0700

    Inventory Update
    Added toolbar, drag and drop feature, item highlight for toolbar and inventory. Added scroll wheel to toolbar

---
+++ 2DTopDownProject/Assets/Scripts/GUI/DragNDrop.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNDrop : MonoBehaviour
+{
+    ItemSlot itemSlot;
+
+    private void Start()
+    {
+        itemSlot = new ItemSlot();
+
+
+    }
+}
---
+++ 2DTopDownProject/Assets/Scripts/GUI/ItemToolbarPanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemToolbarPanel : ItemPanel
+{
+    [SerializeField] ToolBarController toolBarController;
+    public override void OnClick(int id)
+    {
+        toolBarController.Set(id);
+        Highlight(id);
+    }
+    
+    int currentSelectedTool;
+    public void Highlight(int id)
+    {
+        buttons[currentSelectedTool].Highlight(false);
+        currentSelectedTool = id;
+        buttons[currentSelectedTool].Highlight(true);
+    }
+
+    public void Start()
+    {
+        Init();
+        toolBarController.onChange += Highlight;
+        Highlight(0);
+    }
+
+
+
+}
